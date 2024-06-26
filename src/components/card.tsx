f78ba9aea3f13cae34deafbc5ff84359b21d3854
ElecAD <adelektrik3112@gmail.com>
2024-06-26 13:19:37 +0300

    Revision of the Card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,18 +1,17 @@
 import { Button } from "@nextui-org/button";
+import React from "react";
+import { Image } from "@nextui-org/image";
 
 interface CardProps {
-  children?: React.ReactNode;
+  logoSrc?: string;
+  name: string;
 }
 
-export const Card: React.FC<CardProps> = ({ children }) => {
+export const Card: React.FC<CardProps> = ({ logoSrc, name }) => {
   return (
-    // <Button className="bg-transparent font-mono font-semibold h-fit gap-2 border-medium hover:border-indigo-500 hover:text-indigo-800 transition-colors ease-in duration-100 px-3 py-1.5 text-small rounded-medium border-default text-foreground aspect-video">
-    //   {children}
-    // </Button>
-
-  <Button className="bg-transparent font-mono font-semibold h-fit gap-2 border-medium hover:border-indigo-500 hover:text-indigo-800 transition-colors ease-in duration-100 px-3 py-1.5 text-small rounded-medium border-default text-foreground aspect-video">
-    {children}
-  </Button>
-
+    <Button className="whitespace-normal flex-wrap bg-transparent font-mono font-semibold h-fit gap-2 border-medium hover:border-indigo-500 hover:text-indigo-800 px-3 py-1.5 text-small rounded-medium border-default text-foreground aspect-video">
+      <Image className={"border-2"} height={28} src={logoSrc} width={28} />
+      <div>{name}</div>
+    </Button>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,35 @@
-import { Image } from "@nextui-org/image";
-
 import DefaultLayout from "@/layouts/default";
 import { SearchInput } from "@/components/searchInput.tsx";
 import { Card } from "@/components/card.tsx";
 
 export default function IndexPage() {
+  const mock = [
+    {
+      name: "Лента",
+      logoSrc: "lenta.png",
+    },
+    {
+      name: "Магнит",
+      logoSrc: "lenta.png",
+    },
+    {
+      name: "Планета здоровья",
+      logoSrc: "lenta.png",
+    },
+    {
+      name: "Планета здоровья",
+      logoSrc: "lenta.png",
+    },
+  ];
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center">
         <SearchInput />
         <div className="mt-8 grid grid-cols-2 gap-4 w-full">
-          <Card>Порядок</Card>
-          <Card>Магнит</Card>
-          <Card>Планета здоровья</Card>
-          <Card>Пятёрочка</Card>
-          <Card>
-            <Image src="lenta.png" className={"border-medium"} width={28}/>
-            <div>Лента</div>
-          </Card>
+          {mock.map((item, index) => (
+            <Card key={index} logoSrc={item.logoSrc} name={item.name} />
+          ))}
         </div>
       </section>
     </DefaultLayout>
